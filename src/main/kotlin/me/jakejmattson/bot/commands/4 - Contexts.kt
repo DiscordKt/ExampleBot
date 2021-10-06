1187509a0ec09a56a15060177881555e6c40840b
JakeJMattson <dev.ioparameters@simplelogin.fr>
2021-10-06 13:34:20 -0500

    Convert globalCommand example

--- src/main/kotlin/me/jakejmattson/bot/commands/4 - Contexts.kt
+++ src/main/kotlin/me/jakejmattson/bot/commands/4 - Contexts.kt
@@ -5,21 +5,21 @@ import me.jakejmattson.discordkt.api.commands.commands
 //There are different builders for different types of commands.
 //This allows more type-safe access to data, such as channels.
 fun contexts() = commands("Context") {
-    command("Global") {
-        description = "This command can be used in a guild or DM."
-        execute {
-            //This event has a generic MessageChannel and a nullable Guild.
-            val guildName = guild?.name ?: "DM to ${author.tag}"
-            respond("Hello $guildName")
-        }
-    }
-
-    guildCommand("Guild") {
+    command("Guild") {
         description = "This command can only be used in a guild."
         execute {
             //This event has a TextChannel with a non-nullable Guild.
             val channel = channel
             respond("Hello ${channel.name} (${guild.name})")
+        }
+    }
+
+    globalCommand("Global") {
+        description = "This command can be used in a guild or DM."
+        execute {
+            //This event has a generic MessageChannel and a nullable Guild.
+            val guildName = guild?.name ?: "DM to ${author.tag}"
+            respond("Hello $guildName")
         }
     }
 
